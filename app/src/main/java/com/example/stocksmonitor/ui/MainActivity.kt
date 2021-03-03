61b547c073f44be064854c3e23e420b3ccf6bf48
Indira <temkesheva.i@chocolife.kz>
2021-03-03 22:11:39 +0600

    replace main activity to ui

--- app/src/main/java/com/example/stocksmonitor/ui/MainActivity.kt
+++ app/src/main/java/com/example/stocksmonitor/ui/MainActivity.kt
@@ -1,15 +1,18 @@
-package com.example.stocksmonitor
+package com.example.stocksmonitor.ui
 
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.example.stocksmonitor.R
 import com.example.stocksmonitor.databinding.MainActivityBinding
+import org.koin.android.viewmodel.ext.android.viewModel
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private val stocksViewModel: StocksViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,14 +39,19 @@ class MainActivity : AppCompatActivity() {
                 updateButtonState(btnFavourite, btnFavourite.isSelected)
             }
         }
+        stocksViewModel.getStocks()
     }
 
     private fun updateButtonState(button: Button, isSelected: Boolean) {
         if (isSelected) {
-            button.setTextColor(ContextCompat.getColor(this, R.color.black))
+            button.setTextColor(ContextCompat.getColor(this,
+                R.color.black
+            ))
             button.textSize = 28f
         } else {
-            button.setTextColor(ContextCompat.getColor(this, R.color.grey))
+            button.setTextColor(ContextCompat.getColor(this,
+                R.color.grey
+            ))
             button.textSize = 18f
         }
     }
