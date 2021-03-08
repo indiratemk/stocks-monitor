5be6488b2a5f4aa9ae43885876843267625ff30f
Indira <temkesheva.i@chocolife.kz>
2021-03-08 10:33:25 +0600

    add getting mock data

--- app/src/main/java/com/example/stocksmonitor/ui/stocks/StocksFragment.kt
+++ app/src/main/java/com/example/stocksmonitor/ui/stocks/StocksFragment.kt
@@ -8,9 +8,16 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.stocksmonitor.data.models.Stock
 import com.example.stocksmonitor.databinding.StocksFragmentBinding
 import com.example.stocksmonitor.utils.Resource
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
+
 
 class StocksFragment : Fragment() {
 
@@ -33,12 +40,35 @@ class StocksFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         subscribeObservers()
         initUI()
-        stocksViewModel.getStocks()
+//        stocksViewModel.getStocks()
+
+        val json = loadJSONFromAsset()!!
+        val gson = Gson()
+        val itemType = object : TypeToken<List<Stock>>() {}.type
+        val stocks = gson.fromJson<List<Stock>>(json, itemType)
+        stocksAdapter.stocks = stocks
+    }
+
+    // TODO: 3/4/21 REMOVE
+    private fun loadJSONFromAsset(): String? {
+        val json: String?
+        json = try {
+            val `is`: InputStream = activity!!.assets.open("stocks.json")
+            val size: Int = `is`.available()
+            val buffer = ByteArray(size)
+            `is`.read(buffer)
+            `is`.close()
+            String(buffer, Charset.forName("UTF-8"))
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return null
+        }
+        return json
     }
 
     private fun initUI() {
         initRV()
-        binding.refreshLayout.setOnRefreshListener { stocksViewModel.getStocks() }
+//        binding.refreshLayout.setOnRefreshListener { stocksViewModel.getStocks() }
     }
 
     private fun subscribeObservers() {
