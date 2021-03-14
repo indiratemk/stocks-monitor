aaca697b787463985816d99fccb67327fff84c2f
indiratemk <123bylbhf>
2021-03-14 19:33:59 +0300

    fix stock view holder when data is null

--- app/src/main/java/com/example/stocksmonitor/ui/stocks/StockVH.kt
+++ app/src/main/java/com/example/stocksmonitor/ui/stocks/StockVH.kt
@@ -16,8 +16,14 @@ class StockVH(
     private val binding: StockItemBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(stock: Stock) {
+    fun bind(stock: Stock, listener: StockClickListener?) {
         with(binding) {
+            Glide.with(itemView.context)
+                .load(Constants.IMAGES_URL + stock.symbol)
+                .transform(RoundedCorners(itemView.context.resources.getDimensionPixelSize(R.dimen.logo_border_radius)))
+                .placeholder(R.drawable.img_not_found)
+                .error(R.drawable.img_not_found)
+                .into(ivLogo)
             cvStock.setCardBackgroundColor(
                 if (adapterPosition % 2 == 0)
                     ContextCompat.getColor(itemView.context, R.color.grey_100)
@@ -26,10 +32,18 @@ class StockVH(
             )
             tvTicker.text = stock.symbol
             tvCompanyName.text = stock.longName
+            handlePrice(stock)
+        }
+    }
 
+    private fun handlePrice(stock: Stock) {
+        with(binding) {
             val currencyFormatter = when (stock.currency) {
-                "USD" -> NumberFormat.getCurrencyInstance(Locale.US).apply {
+                Constants.USD_CURRENCY -> NumberFormat.getCurrencyInstance(Locale.US).apply {
                     currency = Currency.getInstance(stock.currency)
+                    maximumFractionDigits = 2
+                }
+                null -> NumberFormat.getCurrencyInstance().apply {
                     maximumFractionDigits = 2
                 }
                 else -> NumberFormat.getCurrencyInstance().apply {
@@ -37,27 +51,24 @@ class StockVH(
                     maximumFractionDigits = 2
                 }
             }
-            tvCurrentPrice.text = currencyFormatter.format(stock.regularPrice)
-            tvDayDelta.setTextColor(
-                if (stock.marketChange < 0 || stock.marketChangePercent < 0)
-                    ContextCompat.getColor(itemView.context, R.color.red)
+            stock.regularPrice?.let {
+                tvCurrentPrice.text = currencyFormatter.format(it)
+            }
+            stock.marketChange?.let { marketChange ->
+                val formattedPercent = String.format("%.2f", stock.marketChangePercent)
+                val formattedMarketChange = if (marketChange > 0)
+                    "+" + currencyFormatter.format(stock.marketChange)
                 else
-                    ContextCompat.getColor(itemView.context, R.color.green)
-            )
-            val formattedPercent = String.format("%.2f", stock.marketChangePercent)
-            tvDayDelta.text = itemView.context.getString(R.string.label_day_delta,
-                if (stock.marketChange > 0)
-                    "+${currencyFormatter.format(stock.marketChange)}"
-                else
-                    currencyFormatter.format(stock.marketChange), formattedPercent)
-
-
-            Glide.with(itemView.context)
-                .load(Constants.IMAGES_URL + stock.symbol)
-                .transform(RoundedCorners(itemView.context.resources.getDimensionPixelSize(R.dimen.logo_border_radius)))
-                .placeholder(R.drawable.img_not_found)
-                .error(R.drawable.img_not_found)
-                .into(ivLogo)
+                    currencyFormatter.format(marketChange)
+                tvDayDelta.text = itemView.context.getString(R.string.label_day_delta,
+                        formattedMarketChange, formattedPercent)
+                tvDayDelta.setTextColor(
+                        if (marketChange < 0)
+                            ContextCompat.getColor(itemView.context, R.color.red)
+                        else
+                            ContextCompat.getColor(itemView.context, R.color.green)
+                )
+            }
         }
     }
 }
